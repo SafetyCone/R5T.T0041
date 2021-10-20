6a33fcf05e7e03f0b5d2f70d99550f90f2767825
David Coats <david@getsafetycone.com>
2021-10-20 15:54:28 -0700

    Add missing ExtensionMethodBaseMarker attribute.

--- source/R5T.T0041/Code/Bases/Interfaces/IDirectorySeparator.cs
+++ source/R5T.T0041/Code/Bases/Interfaces/IDirectorySeparator.cs
@@ -1,11 +1,14 @@
 using System;
 
+using R5T.T0039.T002;
+
 
 namespace R5T.T0041
 {
     /// <summary>
     /// Empty interface as base for extension methods.
     /// </summary>
+    [ExtensionMethodBaseMarker]
     public interface IDirectorySeparator
     {
     }
